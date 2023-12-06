d8f1a8955251c302ab4f9c76e8412f06ca4f7188
Servane CHENE <schene@scaleway.com>
2023-12-06 09:47:07 +0100

    day6 little refacto

--- 2023/day6/day6-1.py
+++ 2023/day6/day6-1.py
@@ -1,19 +1,14 @@
 lines = open("input.txt", "r").readlines()
 
-times = lines[0].split(":")[1].split()
-dist = lines[1].split(":")[1].split()
-
-
-def possible_hold_times(t, d):
-    ret = []
-    for i in range(1, t+1):
-        dist = i * (t - i)
-        if dist > d:
-            ret.append(i)
-    return ret
+times = [eval(x) for x in  lines[0].split(":")[1].split()]
+dist = [eval(x) for x in  lines[1].split(":")[1].split()]
 
 result = 1
 for i in range(len(times)):
-    result *= len(possible_hold_times(times[i], dist[i]))
+    m = 0
+    for j in range(1, times[i]+1):
+        if j * (times[i] - j) > dist[i]:
+            m += 1
+    result *= m
 
 print(result)
--- 2023/day6/day6-2.py
+++ 2023/day6/day6-2.py
@@ -5,14 +5,9 @@ dist = int(lines[1].split(":")[1].replace(" ", ""))
 
 # why think of a faster solution when we can just wait a few seconds
 
-def possible_hold_times(t, d):
-    ret = []
-    for i in range(1, t+1):
-        dist = i * (t - i)
-        if dist > d:
-            ret.append(i)
-    return ret
-
-result = 1 * len(possible_hold_times(times, dist))
+result = 0
+for j in range(1, times+1):
+    if j * (times - j) > dist:
+        result += 1
 
 print(result)
